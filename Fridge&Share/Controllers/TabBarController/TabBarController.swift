2e7657e2cb0c21115e000f52a5fa154b1dc6085e
Елизавета Шерман <lizasherpro001@gmail.com>
2023-11-27 23:36:05 +0300

    profile view controller + some changes

--- Fridge&Share/Controllers/TabBarController/TabBarController.swift
+++ Fridge&Share/Controllers/TabBarController/TabBarController.swift
@@ -10,6 +10,9 @@ import UIKit
 final class TabBarController : UITabBarController {
     private enum Constants {
         static let iconHouse = "house"
+        static let iconPerson = "person"
+        static let titleDormitory = "Dormitory"
+        static let titleProfile = "Profile"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +20,19 @@ final class TabBarController : UITabBarController {
         let dormitoryViewController = DormitoryViewController()
         let dormitoryTabBarItem = UITabBarItem()
         dormitoryTabBarItem.image = UIImage(systemName: Constants.iconHouse)
+        dormitoryTabBarItem.title = Constants.titleDormitory
         dormitoryViewController.tabBarItem = dormitoryTabBarItem
         
+        let profileViewController = ProfileViewController()
+        let profileTabBarItem = UITabBarItem()
+        profileTabBarItem.image = UIImage(systemName: Constants.iconPerson)
+        profileTabBarItem.title = Constants.titleProfile
+        profileViewController.tabBarItem = profileTabBarItem
         
         let vc1 = UINavigationController(rootViewController: dormitoryViewController)
-
+        let vc2 = UINavigationController(rootViewController: profileViewController)
         
-        viewControllers = [vc1]
+        viewControllers = [vc1, vc2]
         
     }
 }
